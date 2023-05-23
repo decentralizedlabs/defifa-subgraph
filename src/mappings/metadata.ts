a07362b8782245e92b3a5f6b3041a1652c5c5c73
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-05-23 20:35:57 +0200

    bypass non json ipfs metadata

--- src/mappings/metadata.ts
+++ src/mappings/metadata.ts
@@ -1,4 +1,4 @@
-import { Bytes, dataSource, json } from "@graphprotocol/graph-ts"
+import { Bytes, dataSource, json, log } from "@graphprotocol/graph-ts"
 
 import { TokenMetadata } from "../../generated/schema"
 
@@ -10,21 +10,23 @@ export function handleMetadata(content: Bytes): void {
   let tokenMetadata = new TokenMetadata(tokenFullId)
   tokenMetadata.token = tokenFullId
 
-  const value = json.fromBytes(content).toObject()
+  if (content.toString().startsWith("{")) {
+    const value = json.fromBytes(content).toObject()
 
-  if (value) {
-    const image = value.get("image")
-    const name = value.get("name")
-    const description = value.get("description")
-    const identifier = value.get("identifier")
-    const tags = value.get("tags")
+    if (value) {
+      const image = value.get("image")
+      const name = value.get("name")
+      const description = value.get("description")
+      const identifier = value.get("identifier")
+      const tags = value.get("tags")
 
-    if (name) tokenMetadata.name = name.toString()
-    if (image) tokenMetadata.image = image.toString()
-    if (description) tokenMetadata.description = description.toString()
-    if (identifier) tokenMetadata.identifier = identifier.toBigInt()
-    if (tags)
-      tokenMetadata.tags = tags.toArray().map<string>((tag) => tag.toString())
+      if (name) tokenMetadata.name = name.toString()
+      if (image) tokenMetadata.image = image.toString()
+      if (description) tokenMetadata.description = description.toString()
+      if (identifier) tokenMetadata.identifier = identifier.toBigInt()
+      if (tags)
+        tokenMetadata.tags = tags.toArray().map<string>((tag) => tag.toString())
+    }
   }
 
   tokenMetadata.save()
